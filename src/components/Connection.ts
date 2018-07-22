aa29a597efd11e5eb34be496e78ac5ea2881a816
Domenic Helfenstein <dom.helfenstein@gmail.com>
2018-07-22 14:27:46 +0200

    connections are more powerful

--- src/components/Connection.ts
+++ src/components/Connection.ts
@@ -10,16 +10,46 @@ export class WtfConnection extends AbstractHtmlElement implements IPlumbable {
     public to: string;
     public source: Direction;
     public target: Direction;
+    // public editable = false;
 
     apply(instance: jsPlumbInstance) {
         instance.connect(<any>{
-            uuids: [
-                `${this.from}${toEndpointName(this.source)}`,
-                `${this.to}${toEndpointName(this.target)}`
-            ],
-            editable: true
+            source: this.from,
+            target: this.to,
+            anchors: [ toEndpointName(this.source), toEndpointName(this.target) ],
+            editable: false,
+            connector: ["Flowchart", { stub: [0, 0], gap: 10, cornerRadius: 5, alwaysRespectStubs: true }],
+            endpoint: "Dot",
+            endpointStyle: WtfConnection.endpointPaintStyle,
+            connectorStyle: WtfConnection.connectorPaintStyle,
+            hoverPaintStyle: WtfConnection.endpointHoverStyle,
+            connectorHoverStyle: WtfConnection.connectorHoverStyle
         });
     }
+
+    private static endpointPaintStyle = {
+        stroke: "#7AB02C",
+        fill: "transparent",
+        radius: 7,
+        strokeWidth: 1
+    };
+
+    private static connectorPaintStyle = {
+        strokeWidth: 2,
+        stroke: "#61B7CF",
+        joinstyle: "round",
+        outlineStroke: "white",
+        outlineWidth: 2
+    };
+
+    private static connectorHoverStyle = {
+        stroke: "#216477"
+    };
+
+    private static endpointHoverStyle = {
+        fill: "#216477",
+        stroke: "#216477"
+    };
 }
 
 window.customElements.define('wtf-connection', WtfConnection);
--- src/components/Element.ts
+++ src/components/Element.ts
@@ -8,24 +8,8 @@ import { Direction, toEndpointName } from "./Common";
 export class WtfElement extends AbstractHtmlElement implements IPlumbable {
     private initialHtml: string;
 
-    public endpoints = "['top', 'right', 'bottom', 'left']";
     public top = "";
     public left = "";
-
-    private get endpointArray() {
-        return JSON.parse(this.endpoints.replaceAll("'", "\""));
-    }
-
-    private get anchors() {
-        var result = [];
-
-        for (let i = 0; i < this.endpointArray.length; i++) {
-            const direction = <Direction>this.endpointArray[i];
-            result.push(toEndpointName(direction));
-        }
-
-        return result;
-    }
 
     getHtml() {
         return /*html*/`
@@ -56,61 +40,6 @@ export class WtfElement extends AbstractHtmlElement implements IPlumbable {
 
         this.style.top = `${this.top}px`;
         this.style.left = `${this.left}px`;
-
-        this.anchors.forEach(a => instance.addEndpoint(
-            this,
-            <any>this.endpoint,
-            <any>{
-                anchor: a,
-                isSource: true,
-                isTarget: true,
-                maxConnections: -1,
-                uuid: `${this.id}${a}`
-            }));
-    }
-
-    private static connectorPaintStyle = {
-        strokeWidth: 2,
-        stroke: "#61B7CF",
-        joinstyle: "round",
-        outlineStroke: "white",
-        outlineWidth: 2
-    };
-
-    private static connectorHoverStyle = {
-        strokeWidth: 3,
-        stroke: "#216477",
-        outlineWidth: 5,
-        outlineStroke: "white"
-    };
-
-    private static endpointHoverStyle = {
-        fill: "#216477",
-        stroke: "#216477"
-    };
-
-    private endpoint = {
-        endpoint: "Dot",
-        paintStyle: {
-            stroke: "#7AB02C",
-            fill: "transparent",
-            radius: 7,
-            strokeWidth: 1
-        },
-        isSource: true,
-        connector: ["Flowchart", { stub: [20, 20], gap: 10, cornerRadius: 5, alwaysRespectStubs: true }],
-        connectorStyle: WtfElement.connectorPaintStyle,
-        hoverPaintStyle: WtfElement.endpointHoverStyle,
-        connectorHoverStyle: WtfElement.connectorHoverStyle,
-        dragOptions: {},
-        overlays: [
-            ["Label", {
-                location: [0.5, 1.5],
-                label: "Drag",
-                cssClass: "endpointSourceLabel",
-                visible: false
-            }]
-        ]
     }
 }
 
